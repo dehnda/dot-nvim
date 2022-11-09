63b7f318cf714f40b09c3893fef1aef2d831c86c
Daniel Dehne <danny.dehne@gmail.com>
2022-11-09 10:44:56 +0100

    feat(keybinds): add split movements with leader

--- lua/dehnda/keybinds.lua
+++ lua/dehnda/keybinds.lua
@@ -9,10 +9,16 @@ end
 map('n', '<leader>w', '<CMD>update<CR>')
 map('n', '<leader>W', '<CMD>wall<CR>')
 
+-- Move splits around
+map('n', '<leader>h', '<C-w>h<CR>')
+map('n', '<leader>l', '<C-w>l<CR>')
+map('n', '<leader>k', '<C-w>k<CR>')
+map('n', '<leader>j', '<C-w>j<CR>')
+
 -- Quit neovim
 map('n', '<leader>Q', '<CMD>q<CR>')
 
--- Copying the vscode behaviour of making tab splits
-map('n', '<leader>V', '<CMD>vsplit<CR>')
-map('n', '<leader>H', '<CMD>split<CR>')
+-- Split shortcuts
+map('n', '<leader>[', '<CMD>vsplit<CR>')
+map('n', '<leader>]', '<CMD>split<CR>')
 
